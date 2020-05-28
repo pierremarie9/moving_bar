a8711968e58e24c8d7b99a8dfb1783b9fd808d17
pierremarie9 <63357279+pierremarie9@users.noreply.github.com>
2020-05-28 18:12:06 +0200

    tennis base functions are properly working

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let bar = document.querySelector('#bar');
 let ball = document.querySelector('#ball');
-var moveBy = 10;
+var moveByBar = 10;
+var moveByBall = 5
 
 window.addEventListener('load', ()=>{
     bar.style.position = 'absolute';
@@ -15,12 +16,12 @@ window.addEventListener('keydown', (e)=>{
     switch(e.key){
         case 'ArrowLeft':
             if(parseInt(bar.style.left) > 0){
-                bar.style.left = parseInt(bar.style.left) - moveBy +'px';
+                bar.style.left = parseInt(bar.style.left) - moveByBar +'px';
             }
             break;
         case 'ArrowRight':
             if(parseInt(bar.style.left) < window.innerWidth - 60){
-                bar.style.left = parseInt(bar.style.left) + moveBy +'px';
+                bar.style.left = parseInt(bar.style.left) + moveByBar +'px';
             }
             break;
         // case 'ArrowUp':
@@ -30,7 +31,6 @@ window.addEventListener('keydown', (e)=>{
         //     bar.style.top = parseInt(bar.style.top) + moveBy +'px';
         //     break;
         case ' ':
-            document.body.style.backgroundColor = 'red';
             move(ball);
             break;
     }
@@ -40,31 +40,60 @@ window.addEventListener('keydown', (e)=>{
 function move(item){
     var moveDown = true;
     var moveUp = false;
+    var moveSide = generateStep();
     var id = setInterval(()=>{
 
-        if(moveDown == true){
-            if(parseInt(item.style.top)< window.innerHeight - 25){
-                item.style.top = parseInt(item.style.top) + moveBy + 'px';
+        if(moveDown == true && parseInt(item.style.top)< window.innerHeight - 25){
+            item.style.top = parseInt(item.style.top) + moveByBall + 'px';
+            item.style.left = parseInt(item.style.left) + moveSide + 'px';
+            if(parseInt(item.style.left) < 0){
+                moveSide = -moveSide;
             }
-            else{
+            else if(parseInt(item.style.left) > window.innerWidth - 23){
+                moveSide = -moveSide;
+            }
+        }
+        else if(moveDown == true){
                 moveDown = false;
                 moveUp = true;
-            };
-
         };
 
-        if(moveUp == true){
-            if(parseInt(item.style.top)> 0){
-                item.style.top = parseInt(item.style.top) - moveBy + 'px';
+        if(moveUp == true && parseInt(item.style.top)> 24){
+            item.style.top = parseInt(item.style.top) - moveByBall + 'px';
+            item.style.left = parseInt(item.style.left) + moveSide + 'px';
+            if(parseInt(item.style.left) < 0){
+                moveSide = -moveSide;
             }
-            else{
+            else if(parseInt(item.style.left) > window.innerWidth - 23){
+                moveSide = -moveSide;
+            }
+        }
+        else if(moveUp == true && parseInt(item.style.left ) >= parseInt(bar.style.left) && parseInt(item.style.left) <= parseInt(bar.style.left) + 40){
                 moveUp = false;
                 moveDown = true;
-            };
-
+                moveSide = generateStep();
+        }
+        else if(moveUp == true){
+            let lose = document.createElement('div');
+            lose.textContent = "You lost loser !";
+            lose.style.color = "white";
+            document.body.append(lose);
+            clearInterval(id);
         };
         
- 
-    }, 100);
+    }, 18);
+
+};
+
+function generateStep(){
+    randomOne = Math.random();
+    randomTwo = Math.random();
+
+    if(randomOne < 0.5){
+        return - (randomTwo*7);
+    }
+    else{
+        return randomTwo*7;
+    }
 
 }
